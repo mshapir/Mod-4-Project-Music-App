b3cc0a19e025a65e1bdc4f8b81d1a0057d69f2f6
github username <raquel.sae.randall@gmail.com>
2019-01-19 15:58:07 -0500

    added create user

---
+++ mod-4-proj-music-frontend/src/components/User.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NewUserForm from './NewUserForm';
+
+
+class User extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Users</h1>
+        <NewUserForm
+          newUserSubmitHandler={this.props.newUserSubmitHandler}
+          />
+      </div>
+    )
+  }
+}
+export default User;
