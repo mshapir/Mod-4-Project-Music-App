b146db81d693e01832afe49eba918acaaa72ad77
github username <raquel.sae.randall@gmail.com>
2019-01-21 18:08:41 -0500

    fixed login and signup auth

--- mod-4-proj-music-api/app/controllers/api/v1/auth_controller.rb
+++ mod-4-proj-music-api/app/controllers/api/v1/auth_controller.rb
@@ -4,7 +4,7 @@ skip_before_action :authorized, only: [:create]
   def create
     @user = User.find_by(username: user_login_params[:username])
     #User#authenticate comes from BCrypt
-    if @user && @user.authenticate(user_login_params[:password_digest])
+    if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
       token = encode_token({ user_id: @user.id })
       render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
@@ -24,6 +24,6 @@ skip_before_action :authorized, only: [:create]
   private
 
   def user_login_params
-    params.permit(:username, :password_digest)
+    params.permit(:username, :password)
   end
 end
